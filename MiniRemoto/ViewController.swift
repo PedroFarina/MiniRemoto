bda86a31e3cf564f835a2ce0070fca6ee63172e0
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-05-15 14:33:15 -0300

    Adding project

---
+++ MiniRemoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniRemoto
+//
+//  Created by Pedro Giuliano Farina on 15/05/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
