9cf5ad7c2187fa25210c9f943abaea66bd027831
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-05-19 15:02:49 -0300

    Creating Round View

---
+++ MiniRemoto/View/Adding Event/ColorCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ColorCollectionViewCell.swift
+//  MiniRemoto
+//
+//  Created by Pedro Giuliano Farina on 19/05/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import Foundation
