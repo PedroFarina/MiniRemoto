1a2f90d54692e3f23421ff4e22cc46f9936bf804
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-05-25 16:09:24 -0300

    Adding data to calendar module

--- MiniRemoto/Model/Entities/Modules/CalendarData.swift
+++ MiniRemoto/Model/Entities/Modules/CalendarData.swift
@@ -11,7 +11,29 @@ public class CalendarData: Module {
     public var addImage: UIImage? = UIImage(named: "CalendarAddModule")
     public var removeImage: UIImage? = UIImage(named: "CalendarRemoveModule")
     public var image: UIImage? = UIImage(named: "CalendarModule")
-    public var title: String? = "Data"
-    public var subtitle: String? = "Horário"
-    public var date: Date?
+    public var title: String? = "Data".localized()
+    public var subtitle: String? = "Horário".localized()
+
+    private static let hourDF: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat =  "HH:mm"
+        return df
+    }()
+    private static let dateDF: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "dd/MM/yyyy"
+        return df
+    }()
+
+    public var date: Date? {
+        didSet {
+            if let date = date {
+                title = CalendarData.dateDF.string(from: date)
+                subtitle = CalendarData.hourDF.string(from: date)
+            } else {
+                title = "Data".localized()
+                subtitle = "Horário".localized()
+            }
+        }
+    }
 }
