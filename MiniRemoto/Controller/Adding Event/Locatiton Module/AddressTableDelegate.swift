c40b1d9c64b03051a1abf6171e866c6ed22af00f
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-05-27 15:51:19 -0300

    Adding storybooard

---
+++ MiniRemoto/Controller/Adding Event/Locatiton Module/AddressTableDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AddressTableDelegate.swift
+//  MiniRemoto
+//
+//  Created by Pedro Giuliano Farina on 27/05/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import Foundation
---
+++ MiniRemoto/Controller/Adding Event/Locatiton Module/LocationModuleController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationModuleController.swift
+//  MiniRemoto
+//
+//  Created by Pedro Giuliano Farina on 27/05/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+public class LocationModuleController: UIViewController {
+    
+}
