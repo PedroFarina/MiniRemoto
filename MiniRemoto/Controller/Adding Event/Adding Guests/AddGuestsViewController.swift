0d69758dced45794c9d39106b696740782919cac
eloisafalcao <eloisamariafalcao@gmail.com>
2020-05-27 21:59:32 -0300

    InviteModule implementada

--- MiniRemoto/Controller/Adding Event/Adding Guests/AddGuestsViewController.swift
+++ MiniRemoto/Controller/Adding Event/Adding Guests/AddGuestsViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class AddGuestsViewController: UIViewController {
+class AddGuestsViewController: UIViewController, ModuleController {
+    
+    var module: Module?
+    var reloadData: (() -> Void)?
     
     @IBOutlet weak var contactsCollectionView: UICollectionView!
     @IBOutlet weak var contactsCollectionViewHeight: NSLayoutConstraint!
     @IBOutlet weak var contactsTableView: UITableView!
     @IBOutlet weak var searchBar: CustomSearchBar!
+    @IBOutlet weak var save: UIButton!
+
+    private func checkSave() {
+
+        if returnGuestsIds().count == 0 {
+            save.isEnabled = false
+        } else {
+            save.isEnabled = true
+        }
+    }
     
     var tableViewDataSource: ContactsTableViewDataSource?
     var tableViewDelegate: ContactsTableViewDelegate?
     var collectionViewDataSource: ContactsCollectionViewDataSource?
     var collectionViewDelegate: ContactsCollectionViewDelegate?
-
+    
     let contactsManager = ContactsManager()
     var contacts = [SelectableContact]()
     var searchResp = [SelectableContact]()
@@ -31,13 +44,28 @@ class AddGuestsViewController: UIViewController {
         setupCollectionView()
         setupTableView()
         setupSearch()
+        setupModule()
+        checkSave()
     }
 
+    func setupModule() {
+        guard let inviteModule = module as? InviteData, let ids = inviteModule.guestsIds else { return }
+
+        for id in ids {
+            contacts.forEach { (contact) in
+                if contact.id == id {
+                    contact.isSelected = true
+                }
+            }
+        }
+        checkSave()
+    }
+    
     func setupSearch() {
         searchBar.setup(contacts: contacts, tableView: contactsTableView)
         searchBar.searchDelegate = tableViewDataSource
     }
-
+    
     func setupTableView() {
         tableViewDataSource = ContactsTableViewDataSource(contacts: contacts)
         tableViewDelegate = ContactsTableViewDelegate(collectionView: contactsCollectionView, textField: searchBar, sections: tableViewDataSource?.sections ?? [Section]())
@@ -45,7 +73,7 @@ class AddGuestsViewController: UIViewController {
         contactsTableView.dataSource = tableViewDataSource
         contactsTableView.delegate = tableViewDelegate
     }
-
+    
     func setupCollectionView() {
         collectionViewDataSource = ContactsCollectionViewDataSource(collectionViewHeigh: contactsCollectionViewHeight)
         collectionViewDelegate = ContactsCollectionViewDelegate(tableView: contactsTableView)
@@ -54,18 +82,55 @@ class AddGuestsViewController: UIViewController {
         contactsCollectionViewHeight.constant = 0.0
         collectionViewDelegate?.delegate = self
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
+    }
+
+    func returnGuestsIds() -> [String] {
+        var ids: [String] = []
+
+        contacts.forEach { (contact) in
+            if contact.isSelected == true {
+                ids.append(contact.id)
+            }
+        }
+
+        return ids
+    }
+
+    func setGuestsSelection() {
+        contacts.forEach { (contact) in
+            if contact.isSelected == true {
+                contact.isSelected = false
+            }
+        }
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true)
+        setGuestsSelection()
+    }
+    
+    @IBAction func saveData(_ sender: Any) {
+        guard let data = module as? InviteData else {
+            fatalError("Module was not InviteData")
+        }
+
+        data.guestsIds = returnGuestsIds()
+
+        reloadData?()
+        self.dismiss(animated: true)
     }
 }
 
 extension AddGuestsViewController: UnselectContactInTableView {
-
+    
     func unselect(_ contact: SelectableContact) {
         tableViewDataSource?.unselect(contact)
         collectionViewDataSource?.remove(contact)
         contactsTableView.reloadData()
         contactsCollectionView.reloadData()
+        checkSave()
     }
 }
